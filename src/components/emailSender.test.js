921bc7711d75f1ae5d57dfeef19efa5891e94c0e
Austin Xie <weiyu.xie@gmail.com>
2018-05-10 01:14:36 +1000

    clean test

--- src/components/emailSender.test.js
+++ src/components/emailSender.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import { configure, shallow, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import EmailAddressesInput from './emailAddressesInput';
 import EmailSubject from './emailSubject';
